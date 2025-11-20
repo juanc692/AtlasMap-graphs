3155eb25487c5add37ced1d0ab267312c86f0bdc
juanc692 <picosalavarrietajuandavid50@gmail.com>
2025-11-20 10:41:05 -0500

    Creacion de mapa de grafos

--- Grafos/src/main/java/Grafos/App.java
+++ Grafos/src/main/java/Grafos/App.java
@@ -12,6 +12,9 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/front/grafos.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
+        scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+
         stage.setTitle("Grafos");
         stage.setScene(scene);
         stage.show();
--- Grafos/src/main/java/Grafos/grafos_controlador.java
+++ Grafos/src/main/java/Grafos/grafos_controlador.java
@@ -1,10 +1,69 @@
 package Grafos;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+import java.util.List;
 
 public class grafos_controlador {
+    @FXML private Button btnDistancia;
+    @FXML private Button btnPeajes;
+    @FXML private Button btnRuta;
+
+    @FXML private Text pereira_bogota;
+    @FXML private Text bogota_funza;
+    @FXML private Text funza_villao;
+    @FXML private Text villao_cali;
+    @FXML private Text cali_tunja;
+    @FXML private Text cali_pereira;
+    @FXML private Text medellin_cali;
+    @FXML private Text funza_medellin;
+
+    private Text[] listaKm;
+
     @FXML
     public void initialize()
     {
+        listaKm = new Text[]{
+                pereira_bogota,
+                bogota_funza,
+                funza_villao,
+                villao_cali,
+                cali_tunja,
+                cali_pereira,
+                medellin_cali,
+                funza_medellin
+        };
+        ocultarKM();
+        setBotones();
+    }
 
+    private void setBotones()
+    {
+        btnDistancia.setOnAction(actionEvent -> {
+            if(btnDistancia.getText().equals("Mostrar distancia en km"))
+            {
+                btnDistancia.setText("Ocultar distancia en km");
+                mostrarKM();
+            }else{
+                btnDistancia.setText("Mostrar distancia en km");
+                ocultarKM();
+            }
+        });
+    }
+
+    private void mostrarKM()
+    {
+        for(Text km : listaKm)
+        {
+            km.setVisible(true);
+        }
+    }
+    private void ocultarKM()
+    {
+        for(Text km : listaKm)
+        {
+            km.setVisible(false);
+        }
     }
 }
