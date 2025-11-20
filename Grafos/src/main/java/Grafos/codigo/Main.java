0680dd76cd327b5b131a3d72edaa155f9bf68f9b
juanc692 <picosalavarrietajuandavid50@gmail.com>
2025-11-20 13:41:23 -0500

    grafos funcional

--- Grafos/src/main/java/Grafos/codigo/Main.java
+++ Grafos/src/main/java/Grafos/codigo/Main.java
@@ -3,6 +3,8 @@ package Grafos.codigo;
 import java.util.*;
 
 public class Main {
+    private List<String> aristas;
+    private String costo;
     public static void main(String[] args) {
 
         Node node1 = new Node("Funza");
@@ -45,7 +47,7 @@ public class Main {
 
         boolean usarDistancia = true;  // true = distancia, false = peajes
 
-        graph = calculateShortestPath(graph, node2, usarDistancia); //aca se define el nodo de inicio
+        calculateShortestPath(graph, node2, usarDistancia); //aca se define el nodo de inicio
 
         Node destinoFinal = node6;
 
@@ -54,6 +56,92 @@ public class Main {
         System.out.println("Ruta: " + obtenerRuta(destinoFinal));
 
         System.out.println("Aristas recorridas: " + obtenerAristas(destinoFinal));
+    }
+
+    public Main(String partida, String destino,boolean peaje)
+    {
+        Node node1 = new Node("funza");
+        Node node2 = new Node("medellin");
+        Node node3 = new Node("villao");
+        Node node4 = new Node("cali");
+        Node node5 = new Node("tunja");
+        Node node6 = new Node("pereira");
+        Node node7 = new Node("bogota");
+
+        node1.addDestination(node2, 20, 0);
+        node1.addDestination(node3, 40, 1);
+        node1.addDestination(node7, 40, 1);
+
+        node2.addDestination(node4, 50, 1);
+        node2.addDestination(node1, 20, 0);
+
+        node3.addDestination(node6, 100, 3);
+        node3.addDestination(node1, 40, 1);
+
+        node4.addDestination(node5, 10, 0);
+        node4.addDestination(node2, 50, 1);
+
+        node5.addDestination(node4, 10, 0);
+
+        node6.addDestination(node7, 30, 2);
+        node6.addDestination(node3, 100, 3);
+
+        node7.addDestination(node6, 30, 2);
+        node7.addDestination(node1, 40, 1);
+
+        Graph graph = new Graph();
+        graph.addNode(node1);
+        graph.addNode(node2);
+        graph.addNode(node3);
+        graph.addNode(node4);
+        graph.addNode(node5);
+        graph.addNode(node6);
+        graph.addNode(node7);
+
+        boolean usarDistancia = !peaje;  // true = distancia, false = peajes
+
+        Node nodePartida = null,nodeDestino = null;
+        for(Node node : Arrays.asList(node1,node2,node3,node4,node5,node6,node7))
+        {
+            if(node.getName().equalsIgnoreCase(partida))
+            {
+                nodePartida = node;
+            }
+            if(node.getName().equalsIgnoreCase(destino))
+            {
+                nodeDestino = node;
+            }
+        }
+
+        calculateShortestPath(graph, nodePartida, usarDistancia); //aca se define el nodo de inicio
+
+        Node destinoFinal = nodeDestino;
+
+        //System.out.println("Costo total: " + destinoFinal.getDistance());
+        setCosto(destinoFinal.getDistance().toString());
+        //System.out.println("Ruta: " + obtenerRuta(destinoFinal));
+
+        setAristas(obtenerAristas(destinoFinal));
+        //System.out.println("Aristas recorridas: " + getAristas());
+
+    }
+
+    private void setCosto(String costo)
+    {
+        this.costo = costo;
+    }
+    public String getCosto()
+    {
+        return costo;
+    }
+
+    private void setAristas(List<String> aristas)
+    {
+        this.aristas = aristas;
+    }
+    public List<String> getAristas()
+    {
+        return aristas;
     }
 
     private static Node getLowestDistanceNode(Set<Node> unsettledNodes) {
@@ -123,12 +211,12 @@ public class Main {
         Node prev = path.get(0);
         for (int i = 1; i < path.size(); i++) {
             Node curr = path.get(i);
-            edges.add("(" + prev.getName() + "," + curr.getName() + ")");
+            edges.add(prev.getName() + "_" + curr.getName());
             prev = curr;
         }
 
         // última arista hacia el destino final
-        edges.add("(" + prev.getName() + "," + destino.getName() + ")");
+        edges.add(prev.getName() + "_" + destino.getName());
 
         return edges;
     }
--- Grafos/src/main/java/Grafos/grafos_controlador.java
+++ Grafos/src/main/java/Grafos/grafos_controlador.java
@@ -1,6 +1,10 @@
 package Grafos;
+import Grafos.codigo.Main;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
@@ -8,6 +12,9 @@ import javafx.scene.text.Text;
 import java.util.*;
 
 public class grafos_controlador {
+
+    @FXML private Label labelCostoTotal;
+
     @FXML private Button btnDistancia;
     @FXML private Button btnPeajes;
     @FXML private Button btnRuta;
@@ -41,6 +48,8 @@ public class grafos_controlador {
     private Text[] listaKm;
     private Map<Text,Line> lineas;
     private Set<Circle> seleccionados;
+    private Map<Circle,String> ciudades;
+    private Circle destino;
 
     @FXML
     public void initialize()
@@ -76,6 +85,7 @@ public class grafos_controlador {
                         primerSeleccionado.setStyle("-fx-stroke: green;");
                     }
                     seleccionados.add(ciudad);
+                    destino = ciudad;
 
                     Circle primerSeleccionado = seleccionados.iterator().next(); //esto devolvera el primer elemento en el orden
                     if(circle == primerSeleccionado) {
@@ -90,8 +100,10 @@ public class grafos_controlador {
                     //eliminar estilo /*-fx-stroke: transparent;*/
                     circle.setStyle("-fx-stroke: transparent;");
                     seleccionados.remove(circle);
-                    Circle primerSeleccionado = seleccionados.iterator().next(); //esto devolvera el primer elemento en el orden
-                    primerSeleccionado.setStyle("-fx-stroke: green;");
+                    if(!seleccionados.isEmpty()) {
+                        Circle primerSeleccionado = seleccionados.iterator().next(); //esto devolvera el primer elemento en el orden
+                        primerSeleccionado.setStyle("-fx-stroke: green;");
+                    }
                 }
             });
         }
@@ -116,6 +128,25 @@ public class grafos_controlador {
         lineas.put(pereira_villao,pereira_villao_line);
         lineas.put(medellin_cali,medellin_cali_line);
         lineas.put(funza_medellin,funza_medellin_line);
+
+        pereira_bogota.setUserData("pereira_bogota");
+        bogota_funza.setUserData("bogota_funza");
+        funza_villao.setUserData("funza_villao");
+        villao_cali.setUserData("villao_cali");
+        cali_tunja.setUserData("cali_tunja");
+        pereira_villao.setUserData("pereira_villao");
+        medellin_cali.setUserData("medellin_cali");
+        funza_medellin.setUserData("funza_medellin");
+
+        ciudades = new HashMap<>();
+        ciudades.put(bogota,"bogota");
+        ciudades.put(medellin,"medellin");
+        ciudades.put(cali,"cali");
+        ciudades.put(tunja,"tunja");
+        ciudades.put(pereira,"pereira");
+        ciudades.put(funza,"funza");
+        ciudades.put(villao,"villao");
+
     }
 
     private void setBotones()
@@ -130,6 +161,62 @@ public class grafos_controlador {
                 ocultarKM();
             }
         });
+
+        btnRuta.setOnAction(actionEvent -> {
+            if(seleccionados.size() == 2)
+            {
+                calulcarDistancia(seleccionados.iterator().next(),destino);
+            }else{
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("error");
+                alert.setContentText("Seleccione dos ciudades:\nVerde: Punto de partida\nRojo: punto de destino");
+                alert.showAndWait();
+            }
+        });
+        btnPeajes.setUserData(false);
+        btnPeajes.setOnAction(actionEvent -> {
+            boolean estado = (boolean) btnPeajes.getUserData();
+
+            if (estado) {
+                btnPeajes.setUserData(false);
+                btnPeajes.getStyleClass().remove("peaje");
+            } else {
+                btnPeajes.setUserData(true);
+                btnPeajes.getStyleClass().add("peaje");
+            }
+        });
+
+    }
+
+    private void calulcarDistancia(Circle partida, Circle destino)
+    {
+        Main calculo = new Main(ciudades.get(partida),ciudades.get(destino),(boolean) btnPeajes.getUserData());
+        List<String> aristas = calculo.getAristas();
+
+        labelCostoTotal.setText("Costo: "+calculo.getCosto()+"km");
+
+        for(Map.Entry<Text,Line> l : lineas.entrySet())
+        {
+            l.getValue().setStroke(Color.BLUE);
+        }
+
+        for (String arista : aristas) {
+            for (Map.Entry<Text, Line> linea : lineas.entrySet()) {
+
+                String[] key = ((String) linea.getKey().getUserData()).split("_");
+                String[] ruta = arista.split("_");
+
+                Set<String> setKey = new HashSet<>(Arrays.asList(key));
+                Set<String> setRuta = new HashSet<>(Arrays.asList(ruta));
+
+                if (setKey.equals(setRuta)) {
+                    linea.getValue().setStroke(Color.GREEN);
+                } else {
+//                    linea.getValue().setStroke(Color.BLUE);
+                }
+            }
+        }
+
     }
 
     private void mostrarKM()
