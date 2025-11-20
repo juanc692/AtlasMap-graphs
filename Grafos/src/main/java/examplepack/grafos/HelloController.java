137a7b2226df2da91987bc2dad449695dcdbe551
juanc692 <picosalavarrietajuandavid50@gmail.com>
2025-11-20 09:29:02 -0500

    Creacion del proyecto

---
+++ Grafos/src/main/java/examplepack/grafos/HelloController.java
@@ -0,0 +1,14 @@
+package examplepack.grafos;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
